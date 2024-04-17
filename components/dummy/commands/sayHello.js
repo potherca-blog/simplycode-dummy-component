b9cde4642370335ba0dccc501d7d3f582aaf831a
Ben Peachey <ben@muze.nl>
2024-04-17 19:09:58 +0200

    Change command to use the action.

--- components/dummy/commands/sayHello.js
+++ components/dummy/commands/sayHello.js
@@ -2,4 +2,7 @@ function (element) {
   // @TODO: Subject should eventually come from the HTML element
   const subject = 'World'
   simplyApp.actions.sayHello(subject)
+    .then((message) => {
+    	alert(message)
+    })
 }
